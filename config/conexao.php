f45f246e5cdf27975fabff0e3578c2981d569b38
Kaio Freire <125608754+pouskzin@users.noreply.github.com>
2025-12-16 11:51:20 -0300

    Update conexao.php

--- config/conexao.php
+++ config/conexao.php
@@ -3,13 +3,11 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "pkzsneakers";
-$port = 3307; // <--- ATENÇÃO: Se mudaste o XAMPP para 3307, muda este número para 3307!
+$dbname = "";
+$port = ;
 
-// Criar conexão
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-// Verificar conexão
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
